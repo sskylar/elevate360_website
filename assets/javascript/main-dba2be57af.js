17f76245b8fc38b6f291fee299c7839d20771d70
Grant Blakeman <hello@grantblakeman.com>
2018-11-20 11:32:52 -0800

    header - dropdown - initial js pass

--- assets/javascript/main-dba2be57af.js
+++ assets/javascript/main-dba2be57af.js
@@ -1,13 +1,54 @@
-(function scope() {
-    const nav_node = document.getElementById('nav');
-    const nav_icon = document.getElementById('nav_icon');
-    // const nav_items = document.querySelectorAll('nav > a.nav-item');
+const DropMenu = {
+  init: () => {
+    return DropMenu.watchPrimaryLinks()
+  },
+  watchPrimaryLinks: () => {
+    const openMenu = (e) => {
+        if (e.target.classList.contains('top-level-link')) {
+            const navListElement = e.target.closest('.nav-item')
 
-    nav_icon.onclick = function click() {
-        nav_node.classList.toggle('collapsed')
-        nav_icon.classList.toggle('open')
-        // for (let i = nav_items.length - 1; i >=0; i--) {
-        //     nav_items[i].classList.toggle('')
-        // }
+            navElement.classList.add('expanded')
+            DropMenu.closeAllSections(primayLinks)
+            DropMenu.openPrimarySection(navListElement)
+        }
     }
-}());
+
+    const closeMenu = (e) => {
+        navElement.classList.remove('expanded')
+        DropMenu.closeAllSections(primayLinks)
+    }
+
+    const navElement = document.getElementById('nav')
+    const primayLinks = navElement.querySelectorAll('.primary')[0]
+
+    primayLinks.addEventListener('mouseover', openMenu, false)
+    primayLinks.addEventListener('touchenter', openMenu, false)
+    navElement.addEventListener('mouseleave', closeMenu, false)
+    navElement.addEventListener('touchleave', closeMenu, false)
+  },
+  openPrimarySection: (navListElement) => {
+    navListElement.classList.add('open')
+  },
+  closeAllSections: (primayLinks) => {
+    // close link sets that might be currently open
+    const navListElements = primayLinks.querySelectorAll('.nav-item')
+    if (navListElements) {
+        Array.prototype.forEach.call(navListElements, (el) => {
+          el.classList.remove('open')
+        })
+    }
+  }
+}
+
+ready(DropMenu.init)
+
+// ---------------------------------------------- first-load boilerplate stuff below
+
+// fire once DOM is loaded
+function ready(fn) {
+  if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', fn)
+  } else {
+    fn()
+  }
+}
